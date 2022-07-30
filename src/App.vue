78233f1eb29b22e80a0f2094659da0e4ea4b14fb
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-07-30 18:44:14 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <layout>
+      <template v-slot:header>
+        <h1>Header</h1>
+      </template>
+      <template v-slot:default>
+        <h2>Some content</h2>
+        <p>Some paragraph</p>
+      </template>
+      <template v-slot:footer>
+        <p>Footer</p>
+      </template>
+    </layout>
+  </div>
+</template>
+
+<script>
+import Layout from "./Layout";
+export default {
+  name: "App",
+  components: {
+    Layout,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.np-credits {
+  font-size: 12px;
+  margin-top: 12px;
+  color: #4b4b4b;
+}
+</style>
---
+++ src/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <header class="header-slot">
+      <slot name="header"></slot>
+    </header>
+    <main class="main-slot">
+      <slot></slot>
+    </main>
+    <footer class="footer-slot">
+      <slot name="footer"></slot>
+      <div class="np-credits">wwww.nightprogrammer.com</div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+};
+</script>
+
+<style scoped>
+.header-slot {
+  background: #eee;
+  padding: 10px 6px;
+}
+.main-slot {
+  padding: 16px 6px;
+}
+.footer-slot {
+  background: #eee;
+  padding: 10px 6px;
+}
+</style>
